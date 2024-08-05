9b5926596a121f957b517f0d22658fefb02d36b8
DieserMastro <117863339+DieserMastro@users.noreply.github.com>
2024-08-05 17:47:26 +0200

    yurr

---
+++ Assets/Scripts/Game_Controller.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManager;
+
+public class Game_Controller : MonoBehaviour
+{
+    public enum gameState
+    {
+        MAIN_MENU,
+        PAUSE,
+        BAR,
+        LAPTOP,
+        LAB,
+
+    }
+    [SerializeField]
+    private List<> connectedScenes;
+
+
+    void Start()
+    {
+        
+    }
+
+    
+}
